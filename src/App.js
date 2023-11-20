f23ab57a3f501360ee05ad65f6d5ed2005855ea2
thoaikieu99 <nogamenolife1508@gmail.com>
2023-11-20 23:43:35 +0700

    bai17

--- src/App.js
+++ src/App.js
@@ -2,23 +2,42 @@ import logo from './logo.svg';
 import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
-const App = () => {
-  const count = useSelector(state => state.counter.count);
-  const dispatch = useDispatch();
+import Mycomponent from './components/MyComponent';
+import React from 'react';
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello
-        </p>
-        <div>Count = {count}</div>
-        <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-        <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        hello
+        <Mycomponent></Mycomponent>
+      </div>
+    )
+  }
 }
+
+// const App = () => {
+//   const count = useSelector(state => state.counter.count);
+//   const dispatch = useDispatch();
+
+//   return (
+//     <div>
+//       hellos
+//       <Mycomponent></Mycomponent>
+//     </div>
+
+//     // <div className="App">
+//     //   <header className="App-header">
+//     //     <img src={logo} className="App-logo" alt="logo" />
+//     //     <p>
+//     //       Hello
+//     //     </p>
+//     //     <div>Count = {count}</div>
+//     //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
+//     //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+//     //   </header>
+//     // </div>
+//   );
+// }
 
 export default App;
---
+++ src/components/MyComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+class Mycomponent extends React.Component {
+    render() {
+        return (
+            <div>
+                sssss
+            </div>
+        )
+    }
+}
+export default Mycomponent;
